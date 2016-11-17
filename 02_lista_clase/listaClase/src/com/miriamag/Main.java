6b96008785c4128d245142fbb9b9f4c4ca230567
Unknown <gitclase@gmail.com>
2016-11-17 21:40:33 +0100

    Ejercicio 2 corregido. Completo.

--- 02_lista_clase/listaClase/src/com/miriamag/Main.java
+++ 02_lista_clase/listaClase/src/com/miriamag/Main.java
@@ -16,34 +16,45 @@ public class Main {
                 Nota: El programa termina cuando se escriba FIN como búsqueda.*/
 
 
+         final int ALUMNOS = 3;// Es una constante que se repite en el programa, si se cambia aquí se cambia en todas.
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            String []  listaClase = new String[ALUMNOS];
 
 
+            for (int i = 0; i < ALUMNOS; i++) {// es un bucle for porque recorremos todo el array
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String []  listaClase = new String[20];
+                System.out.println("Escribe Nombre y Apellidos: ");
+                String nombreApellido  = br.readLine();
 
-        for ( int i = 0; i <3 ; i++) {
+                listaClase[i] = nombreApellido;
 
-            System.out.println("Escribe Nombre y Apellidos: ");
-            String nombreApellido = br.readLine();
+            }//el array + los nombres con los que se llena
 
-            listaClase[i]=nombreApellido;
+        String texto;
 
-        }
-
-        System.out.println("Escriba una letra: ");
-
-        char letra = br.readLine().charAt(0);
-
-        for (int x = 0; x < 3; x++) {
-            if (listaClase[x].charAt(0)==letra) {
-                System.out.print(listaClase[x]);
-
-            }
+            do {
 
 
+                System.out.println("Escriba una letra o FIN para terminar: ");
 
-        }
+                texto = br.readLine().toLowerCase();/*necesitamos un string no un char porque
+                 tiene que escribir escribir FIN.
+                 Además se cambia todo a letra minusculas*/
 
+                for (int x = 0; x < ALUMNOS; x++) {
+
+                    if (!texto.equalsIgnoreCase("FIN")) {//el if es para que no te saque la lista con la f cuando escribes fin
+                        char letra = texto.charAt(0);/*Creamos variable letra para poder comparar la letra que ponga con la primera letra del string del array*/
+                        if (listaClase[x].toLowerCase().charAt(0) == letra) {/*aquí las comparaciones debes comparar lo que hay en la lista en minus*/
+                            System.out.println(listaClase[x]);
+
+                        }//muestra todos los nombres que empiecen por la letra que elija el user
+
+                    }
+                }
+            } while (!texto.equalsIgnoreCase("FIN"));//ignore case para que no importe si es minus o mayus
+
+        System.out.println("Hasta luego.");
     }
 }
