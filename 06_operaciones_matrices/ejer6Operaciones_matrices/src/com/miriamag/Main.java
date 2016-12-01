4a7dfacfd0f0a11acf22d2daad1dc294f27c8c0a
Unknown <gitclase@gmail.com>
2016-12-01 19:38:53 +0100

    Ejercicio 6. Estructura hecha. Sin cálculos.

--- 06_operaciones_matrices/ejer6Operaciones_matrices/src/com/miriamag/Main.java
+++ 06_operaciones_matrices/ejer6Operaciones_matrices/src/com/miriamag/Main.java
@@ -1,14 +1,17 @@
 package com.miriamag;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Random;
 
 public class Main {
 
     public static void visualizar(int [][]m){
 
-        for (int i = 0; i < m.length; i++) {
+        for (int i = 0; i < m.length; i++) {//el .length devuelve el numero de filas
 
-            for (int j = 0; j < m.length; j++) {
+            for (int j = 0; j < m[0].length; j++) {//m[0] entra en la primera fila y el .length cuenta la cantidad de huecos que hay a partir de esa posición
 
                 System.out.format("%3d", m[i][j]);
             }
@@ -23,19 +26,33 @@ public class Main {
         int i;
         int j;
 
-        int [][]m3= new int[4][4];
+        int [][]resultado= new int[4][4];
         for (i = 0; i < 4; i++) {
             for (j = 0; j < 4; j++) {
 
-               m3[i][j] =m1[i][j]+ m2[i][j];
+               resultado[i][j] =m1[i][j]+ m2[i][j];
 
             }
 
         }
-        return m3;
+        return resultado;
+    }
+    public static int[][] productoEscalar(int [][]m,int x){
+        int [][] resultado = new int[4][4];
+        return resultado;
+    }
+    public static int [][]producto(int[][]m1, int[][]m2){
+        int [][] resultado = new int[4][4];
+        return resultado;
     }
 
-    public static void main(String[] args) {
+    public static int [][]traspuesta(int[][]m1){
+        int [][] resultado = new int[4][4];
+        return resultado;
+    }
+
+
+    public static void main(String[] args) throws IOException {
 
 	/*Haz un programa que genere dos matrices aleatorias de 4x4 elementos y que muestre:
 
@@ -50,7 +67,7 @@ public class Main {
     Aquí hay un poco de ayuda sobre las matrices y sus operaciones.*/
 
 
-        //las matrices creadas
+        //las matrices creadas y con sus recorridos
 
 	    int [][]matrizLlorar = new int[4][4];
 
@@ -69,24 +86,40 @@ public class Main {
             }
         }
 
+        //llamar a la función visualizar para sacar la matriz
+
+        System.out.println("matriz1: ");
         visualizar(matrizLlorar);
+
+        System.out.println("matriz2: ");
         visualizar(matrizSuicidio);
 
         //sumar matrices
 
-        int[][] r = sumarmatrices(matrizLlorar, matrizSuicidio );
+        /*int[][] matrizMuerte = sumarmatrices(matrizLlorar, matrizSuicidio );*/
+        System.out.println("Suma de matriz1 y matriz2: ");
 
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 4; j++) {
+        visualizar(sumarmatrices(matrizLlorar,matrizSuicidio));//es la matriz resultado de la suma, visualiza la suma de...
 
-                System.out.format("%4d",r[i][j]);
-            }
-            System.out.println();
-        }
+        //producto de un escalar
 
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Escribe un número entero: ");
+        int n = Integer.parseInt(br.readLine());
 
+        System.out.println("producto de matriz1 * numero: ");
+        visualizar(productoEscalar(matrizLlorar,n));//la muestro directamente
 
+        // producto de matrices
 
+        System.out.println("Producto de matriz 1 * matriz 2: ");
+        visualizar(producto(matrizLlorar,matrizSuicidio));
+
+        //traspuesta
+
+        System.out.println("traspuesta de la matriz1: ");
+
+        visualizar(traspuesta(matrizLlorar));
 
 
     }
