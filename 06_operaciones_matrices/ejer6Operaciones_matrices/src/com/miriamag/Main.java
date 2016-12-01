d900bdd305499a0f8a1bd6ca1473eb92ef6728ac
Unknown <gitclase@gmail.com>
2016-12-01 19:08:17 +0100

    Ejercicio 6. Visualizar hecho. Mejoras generales.

--- 06_operaciones_matrices/ejer6Operaciones_matrices/src/com/miriamag/Main.java
+++ 06_operaciones_matrices/ejer6Operaciones_matrices/src/com/miriamag/Main.java
@@ -3,6 +3,21 @@ package com.miriamag;
 import java.util.Random;
 
 public class Main {
+
+    public static void visualizar(int [][]m){
+
+        for (int i = 0; i < m.length; i++) {
+
+            for (int j = 0; j < m.length; j++) {
+
+                System.out.format("%3d", m[i][j]);
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+
     public static int[][] sumarmatrices (int[][] m1,int[][] m2){
 
         int i;
@@ -14,7 +29,6 @@ public class Main {
 
                m3[i][j] =m1[i][j]+ m2[i][j];
 
-               // System.out.format("%4d",m3[i][j]);
             }
 
         }
@@ -25,16 +39,18 @@ public class Main {
 
 	/*Haz un programa que genere dos matrices aleatorias de 4x4 elementos y que muestre:
 
-La suma de las dos matrices.
-El producto de la primera por un escalar (se lo pediremos al usuario).
-El producto de las dos matrices.
-La traspuesta de la segunda matriz.
-Cada operación estará contenida en una función a la que llamaremos desde el programa principal pasándole los datos que necesite.
+    La suma de las dos matrices.
+    El producto de la primera por un escalar (se lo pediremos al usuario).
+    El producto de las dos matrices.
+    La traspuesta de la segunda matriz.
+    Cada operación estará contenida en una función a la que llamaremos desde el programa principal pasándole los datos que necesite.
 
-Una vez que se hayan hecho todos los cálculos, mostraremos los resultados llamando a la función visualizarMatriz() las veces que sea necesario.
+    Una vez que se hayan hecho todos los cálculos, mostraremos los resultados llamando a la función visualizarMatriz() las veces que sea necesario.
 
-Aquí hay un poco de ayuda sobre las matrices y sus operaciones.*/
+    Aquí hay un poco de ayuda sobre las matrices y sus operaciones.*/
 
+
+        //las matrices creadas
 
 	    int [][]matrizLlorar = new int[4][4];
 
@@ -46,42 +62,15 @@ Aquí hay un poco de ayuda sobre las matrices y sus operaciones.*/
             for (int j = 0; j < 4; j++) {
 
                 Random r = new Random ();
-                int divina = r.nextInt(16);
+                int divina = r.nextInt(10);
                 matrizLlorar[i][j]=  divina;
 
-                int providencia = r.nextInt(16);
-                matrizSuicidio[i][j]= providencia;
-
+                matrizSuicidio[i][j]= r.nextInt(10);//las dos formas funcionan igual
             }
-        }//las matrices creadas
-
-
-        for (int i = 0; i < 4; i++) {
-
-            for (int j = 0; j < 4; j++) {
-
-                System.out.format("%4d", matrizLlorar[i][j]);
-
-            }
-           System.out.println();//mostrar las dos matrices
         }
-        System.out.println();
 
-        for (int i = 0; i < 4; i++) {
-
-            for (int j = 0; j < 4; j++) {
-
-                System.out.format("%4d", matrizSuicidio[i][j]);
-
-            }
-            System.out.println();//mostrar las dos matrices
-        }
-        System.out.println();
-
-
-        int [][]matrizMuerte = new int [4][4];
-
-
+        visualizar(matrizLlorar);
+        visualizar(matrizSuicidio);
 
         //sumar matrices
 
@@ -94,6 +83,7 @@ Aquí hay un poco de ayuda sobre las matrices y sus operaciones.*/
             }
             System.out.println();
         }
+
 
 
 
