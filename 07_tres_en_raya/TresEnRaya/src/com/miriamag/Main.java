df2cda8307ff8f439bd6b3a1c3ad4c027daa35a7
Unknown <gitclase@gmail.com>
2016-12-05 21:39:58 +0100

    Ejercicio 7. Terminado

--- 07_tres_en_raya/TresEnRaya/src/com/miriamag/Main.java
+++ 07_tres_en_raya/TresEnRaya/src/com/miriamag/Main.java
@@ -1,29 +1,70 @@
 package com.miriamag;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
 
-    public static void visualizarTablero(int [][]m){
+    public static void visualizarTablero(int [][]m) {
 
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
 
-            if (m[i][j]==0){
-                System.out.println(".");
-            }if (m[i][j]==1){
-                    System.out.println("X");
-                }if(m[i][j]==2){
-                    System.out.println("O");
+                switch (m[i][j]){
+
+                    case 0:
+                        System.out.print(" O ");
+                        break;
+                    case 1:
+                        System.out.print(" X ");
+                        break;
+                    case 7:
+                        System.out.print(" . ");
+                        break;
+
                 }
             }
-
-            }
-
-
+            System.out.println();
         }
+    }
 
 
+public static boolean comprobarGanador(int [][]partida){
 
-    public static void main(String[] args) {
+    for (int i = 0; i < 2; i++) {
+
+
+        if (partida[0][0] == i && partida[0][1] == i && partida[0][2] == i) {
+            return true;
+        }
+        if (partida[1][0] == i && partida[1][1] == i && partida[1][2] == i) {
+            return true;
+        }
+        if (partida[2][0] == i && partida[2][1] == i && partida[2][2] == i) {
+            return true;
+        }
+        if (partida[0][0] == i && partida[1][0] == i && partida[2][0] == i) {
+            return true;
+        }
+        if (partida[0][1] == i && partida[1][1] == i && partida[2][1] == i) {
+            return true;
+        }
+        if (partida[0][2] == i && partida[1][2] == i && partida[2][2] == i) {
+            return true;
+        }
+        if (partida[0][0] == i && partida[1][1] == i && partida[2][2] == i) {
+            return true;
+             }
+        if (partida[0][2] == i && partida[1][1] == i && partida[2][2] == i) {
+            return true;
+            }
+    }
+    
+    return false;
+}
+
+    public static void main(String[] args) throws IOException {
 
     /*Queremos escribir un programa que permita jugar al tres en raya a dos jugadores.
 
@@ -38,13 +79,56 @@ public class Main {
     int ter [][]= new int[3][3];
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
-                ter [i][j]= 0;
-
+                ter[i][j]=7;
             }
-
         }
 
-        visualizarTablero(ter);
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            //visualizamos el tablero
+            visualizarTablero(ter);
+
+            //ver a quien le toca jugar
+            int turno = 0;
+
+            //cuántas tiradas llevamos
+            int tiradas = 0;
+
+        do {
+            //el usuario elige casilla
+            System.out.println("Elige fila:0-2  ");
+            int fila = Integer.parseInt(br.readLine());
+            System.out.println("Elige columna: 0-2");
+            int columna = Integer.parseInt(br.readLine());
+
+            //tirada válida?
+            if (ter[fila][columna] == 7) {
+                ter[fila][columna] = turno;
+
+                //se suma una tirada al contador si la tirada es válida
+                tiradas += 1;
+            //cambiamos de turno
+
+            if (turno == 0){
+                turno = 1;
+            }else {
+                turno = 0;
+            }
+            visualizarTablero(ter);
+
+            }else {
+                System.out.println("POSICIÓN OCUPADA");
+            }
+
+        }while (tiradas < 9 && !comprobarGanador(ter));
+
+        if (tiradas < 9){
+            System.out.println("Has ganado!");
+
+        }else   {
+            System.out.println("Empate");
+        }
+
+
 
     }
 }
