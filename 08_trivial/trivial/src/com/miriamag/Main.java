2f060962727e4b6f7dc5bb3d8054cda86b0a570f
Unknown <gitclase@gmail.com>
2016-12-12 19:12:41 +0100

    Ejercicio 8. Sin generar aleatorias.

--- 08_trivial/trivial/src/com/miriamag/Main.java
+++ 08_trivial/trivial/src/com/miriamag/Main.java
@@ -1,8 +1,13 @@
 package com.miriamag;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Random;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 	/*Escribe un programa que permita jugar al Trivial.
 
     Algunos consejos:
@@ -15,15 +20,49 @@ public class Main {
 
 	//creamos las dos arrays de string
 
-        String preguntas [] = new String [1];
+        String preguntas [] = new String [2];
 
-        String respuestaCorrecta [] = new String [1];
+        String respuestaCorrecta [] = new String [2];
 
         preguntas[0] = "¿Hola?";
+
         preguntas[1] = "¿Qué tal?";
+
 
         respuestaCorrecta[0] = "Hola";
         respuestaCorrecta[1] = "Bien";
 
+        int contadorCorrectas=0;
+        int contadorIncorrectas=0;
+
+        Random posicion = new Random();
+        String terminar;
+
+        do {
+
+
+    System.out.println(preguntas[0]);
+    System.out.println("Introduzca la respuesta: ");
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    String respuestas = br.readLine();
+
+    System.out.println("¿ " + respuestas + ", es lo mismo que: " + respuestaCorrecta[0] + " ? Escribe si o no.");
+
+    String correcta = br.readLine();
+
+    if (correcta.equals("Si")) {
+
+        contadorCorrectas += 1;
+    } else {
+
+        contadorIncorrectas += 1;
+    }
+        System.out.println("¿Quiere terminar?");
+        terminar = br.readLine();
+
+    }while (terminar.equals("No"));
+
+
+        System.out.println("Incorrectas: "+contadorIncorrectas+" Correctas: "+contadorCorrectas);
     }
 }
